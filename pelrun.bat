623df7100abb956058e951935185f287c1bba328
znoop333 <znoop333@hotmail.com>
2019-06-07 08:23:23 -0400

    book-review-Never-Lost-Again.html

--- pelrun.bat
+++ pelrun.bat
@@ -1,1 +1,2 @@
-pelican source --debug --autoreload  --output output --settings pelicanconf.py 
+::pelican source --debug --autoreload  --output output --settings pelicanconf.py 
+pelican source --debug   --output output --settings pelicanconf.py 
